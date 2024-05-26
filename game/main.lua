f22d47d60a9677783bca22b90dd8c0420b5de223
MarcusKolvador <marcuskolvador@gmail.com>
2024-05-26 15:07:42 +0200

    movement plus code cleanup

--- game/main.lua
+++ game/main.lua
@@ -8,11 +8,13 @@ if IS_DEBUG then
 end
 
 function love.load()
+    x = 100 
 end
 
 function love.update()
-	print("Hello!")
+    x = x + 5
 end
 
 function love.draw()
+    love.graphics.rectangle("line", x, 50, 200, 150)
 end
